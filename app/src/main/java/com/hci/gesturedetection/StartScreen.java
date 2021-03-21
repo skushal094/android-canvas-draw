4d3475f97363502fffe2b0609c149ad1041ea294
Ridham <ridhamshah1025@gmail.com>
2021-03-21 14:55:59 +0530

    Add 2 activities, 1 is start screen 2 is for instruction

---
+++ app/src/main/java/com/hci/gesturedetection/StartScreen.java
@@ -0,0 +1,28 @@
+package com.hci.gesturedetection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class StartScreen extends AppCompatActivity {
+Timer timer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(StartScreen.this, Instruction.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+
+}
